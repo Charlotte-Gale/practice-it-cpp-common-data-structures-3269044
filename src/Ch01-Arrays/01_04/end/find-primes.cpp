7cbec498e10d748e7f3925fcc221fe58510a7689
Charlotte-Gale <charlotte.gale@hawkeyeinnovations.com>
2026-04-02 08:07:02 +0000

    completes challenge 2

--- src/Ch01-Arrays/01_04/end/find-primes.cpp
+++ src/Ch01-Arrays/01_04/end/find-primes.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+bool isPrime(int num)
+{
+    if (num < 2) return false;
+
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0) return false;
+    }
+
+    return true;
+}
 
 void findPrimes(const int arr[], size_t size, std::vector<int> &primes)
 {
-    // TODO: Implement
+    for (size_t i = 0; i < size; i++) 
+    {
+        if (isPrime(arr[i]))
+        {
+            primes.push_back(arr[i]);
+        }
+    }
 }
 
 int main()
@@ -13,22 +29,22 @@ int main()
     int numbers[] {4, 7, 12, 3, 9, 17, 29};
     
     size_t size = sizeof(numbers) / sizeof(numbers[0]);
-    vector<int> primes;
+    std::vector<int> primes;
 
     findPrimes(numbers, size, primes);
 
     if (primes.empty())
     {
-        cout << "No prime numbers found in the array.";
+        std::cout << "No prime numbers found in the array.";
     }
     else
     {
-        cout << "The prime numbers in the array are:";
+        std::cout << "The prime numbers in the array are:";
         for (int prime : primes)
         {
-            cout << " " << prime;
+            std::cout << " " << prime;
         }
-        cout << endl;
+        std::cout << std::endl;
     }
     return 0;
 }
