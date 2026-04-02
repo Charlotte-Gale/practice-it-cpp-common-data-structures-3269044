4bdea9dabd71f4ce7e349b20fd67b4fb92c7c93a
Charlotte-Gale <charlotte.gale@hawkeyeinnovations.com>
2026-04-02 07:42:43 +0000

    complete challenge 1

--- src/Ch01-Arrays/01_02/end/sum-array.cpp
+++ src/Ch01-Arrays/01_02/end/sum-array.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int calculateSum(const int arr[], size_t size)
 {
-    //...TODO: implement
+    if (size == 0) {
+        return 0;
+    }
+
+    if (size == 1) {
+        return arr[0];
+    }
+    
+    int result = 0;
+
+    for (size_t i = 0; i < size; i++) {
+        result += arr[i];
+    }
+
+    return result;
 }
 
 int main()
